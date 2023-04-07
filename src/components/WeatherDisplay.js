59b41293590d11d8d3d5256d2574f2dcce477f3b
andrea-maiolo <maiolo.m.2@gmail.com>
2023-04-07 19:38:02 +0200

    working on error message if no location comes back from api

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -21,6 +21,7 @@ const WeatherDisplay = () => {
     );
     const data = await response.json();
     setWeatherData(data);
+    console.log(weatherData);
     setTimeZone(data.timezone);
     setUnixTimeStamp(data.dt);
     const secondeResponse = await fetch(
@@ -35,6 +36,8 @@ const WeatherDisplay = () => {
     fetchWeatherData(location);
   }, [weatherK]);
 
+  console.log(weatherData);
+
   const handleLocationChange = (event) => {
     let valueToCheck = event.target.value;
     valueToCheck =
@@ -47,9 +50,6 @@ const WeatherDisplay = () => {
     let res2 = /[\/`¬!"£$%^&*()_+=\-\/|\\<>\~}{@:{\[\];'#.\/]/g.test(
       valueToCheck
     );
-    console.log(res);
-    console.log(res2, "res2");
-    console.log(isInputCorrect);
     //remove spaces
     let stringWithNoSpaces = valueToCheck.replace(/\s+/g, "");
 
@@ -91,6 +91,9 @@ const WeatherDisplay = () => {
   dailyArrayWithId.shift();
 
   const dailyDom = dailyArrayWithId.map((day) => {
+    if (day.message) {
+      return;
+    }
     return (
       <Daily
         key={day.id}
@@ -105,6 +108,9 @@ const WeatherDisplay = () => {
   hourlyArrayWithId.shift();
 
   const hourlyDom = hourlyArrayWithId.map((hour) => {
+    if (hour.message) {
+      return;
+    }
     return (
       <Hourly
         key={hour.id}
@@ -117,61 +123,74 @@ const WeatherDisplay = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Location:
-          <input
-            type="text"
-            value={location}
-            required
-            minLength="3"
-            maxLength="15"
-            onChange={handleLocationChange}
-          ></input>
-        </label>
-        <button type="submit">
-          Search
-          <img
-            alt="search icon"
-            width={15}
-            height={15}
-            src="https://www.pinclipart.com/picdir/middle/395-3952831_search-search-icon-vector-png-clipart.png"
-          />
-        </button>
-        <p>
-          for a more precise location or if you don't find what you're looking
-          for enter ",COUNTRY" ex. `Rome,IT`
-        </p>
-      </form>
-
-      <div>
-        {isInputCorrect ? (
-          <div className="info">
-            <h2>{name}</h2>
-            <p>{weather[0].main}</p>
-            <p>{weather[0].description}</p>
-            <p>Temperature: {main.temp}°C</p>
-            <p>Feels like: {main.feels_like}°C</p>
-            <p>Humidity:{main.humidity}%</p>
+      <div className="formAndAll">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Location:
+            <input
+              type="text"
+              value={location}
+              required
+              minLength="3"
+              maxLength="15"
+              onChange={handleLocationChange}
+            ></input>
+          </label>
+          <button type="submit">
+            Search
             <img
-              src={`http://openweathermap.org/img/w/${weather[0].icon}.png`}
-              alt={weather[0].description}
+              alt="search icon"
+              width={15}
+              height={15}
+              src="https://www.pinclipart.com/picdir/middle/395-3952831_search-search-icon-vector-png-clipart.png"
             />
-            <p>Precipitations: {Math.floor(daily[0].pop * 100)}%</p>
-
-            <div>{myDate ? <h1>{myDate}</h1> : <p>loading time</p>}</div>
-            {/* <p>{dailyDom}</p> */}
-            {/* <p>{hourlyDom}</p> */}
+          </button>
+          <p>
+            for a more precise location or if you don't find what you're looking
+            for enter ",COUNTRY" ex. `Rome,IT`
+          </p>
+        </form>
+      </div>
+      <div>
+        {weatherData.message ? (
+          <div className="noResponseFromApi">
+            <h2>
+              there was no result for your search, check for any spelling
+              mistakes
+            </h2>
           </div>
         ) : (
           <div>
-            <h2>
-              Input is incorrect, check if it contains numbers or special
-              characters
-            </h2>
+            {isInputCorrect ? (
+              <div className="info">
+                <h2>{name}</h2>
+                <p>{weather[0].main}</p>
+                <p>{weather[0].description}</p>
+                <p>Temperature: {main.temp}°C</p>
+                <p>Feels like: {main.feels_like}°C</p>
+                <p>Humidity:{main.humidity}%</p>
+                <img
+                  src={`http://openweathermap.org/img/w/${weather[0].icon}.png`}
+                  alt={weather[0].description}
+                />
+                <p>Precipitations: {Math.floor(daily[0].pop * 100)}%</p>
+
+                <div>{myDate ? <h1>{myDate}</h1> : <p>loading time</p>}</div>
+                {/* <p>{dailyDom}</p> */}
+                {/* <p>{hourlyDom}</p> */}
+              </div>
+            ) : (
+              <div>
+                <h2>
+                  Input is incorrect, check if it contains numbers or special
+                  characters
+                </h2>
+              </div>
+            )}
           </div>
         )}
       </div>
+      );
     </div>
   );
 };

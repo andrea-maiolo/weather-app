e0ee213388a55659096e59885d68261c2b93448a
andrea-maiolo <maiolo.m.2@gmail.com>
2023-04-12 18:21:30 +0200

    now any type of error is catched and displayed

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -5,73 +5,69 @@ import Daily from "./Daily";
 import Hourly from "./Hourly";
 
 const WeatherDisplay = () => {
+  const weatherK = "20f7632ffc2c022654e4093c6947b4f4";
   const [weatherData, setWeatherData] = useState(null);
   const [hourlyDailyWeather, setHourlyDailyWeather] = useState(null);
-  const weatherK = "20f7632ffc2c022654e4093c6947b4f4";
   const [location, setLocation] = useState("Bologna,IT");
-  const [timeZone, setTimeZone] = useState("");
-  const [unixTimestamp, setUnixTimeStamp] = useState("");
+  const [timezoneFromData, setTimezoneFromData] = useState("");
+  const [unixtimestampFromData, setUnixtimestampFromData] = useState("");
   const [myDate, setMyDate] = useState("");
-  const [isInputCorrect, setIsInputCorrect] = useState(true);
+  const [error, setError] = useState(null);
 
   const fetchWeatherData = async (where) => {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${where}&APPID=${weatherK}&units=metric`,
-      { mode: "cors" }
-    );
-    const data = await response.json();
-    setWeatherData(data);
-    console.log(weatherData);
-    setTimeZone(data.timezone);
-    setUnixTimeStamp(data.dt);
-    const secondeResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={alerts}&appid=${weatherK}`
-    );
-    const secondData = await secondeResponse.json();
-    setHourlyDailyWeather(secondData);
+    //check if non letter char are been typed
+    if (
+      /\d/g.test(where) ||
+      /[\/`¬!"£$%^&*()_+=\-\/|\\<>\~}{@:{\[\];'#.\/]/g.test(where)
+    ) {
+      setError("City name cannot contain numbers or special characters");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${where}&APPID=${weatherK}&units=metric`,
+        { mode: "cors" }
+      );
+      const data = await response.json();
+      //check for error 404
+      if (data.cod === "404") {
+        setError("city not found, please try again");
+        setWeatherData(null);
+      } else {
+        setError(null);
+        setWeatherData(data);
+        setTimezoneFromData(data.timezone);
+        setUnixtimestampFromData(data.dt);
+      }
+
+      const secondeResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={alerts}&appid=${weatherK}`
+      );
+      const secondData = await secondeResponse.json();
+      setHourlyDailyWeather(secondData);
+    } catch (err) {
+      setError("An error occured, please try again");
+      setWeatherData(null);
+    }
   };
 
   useEffect(() => {
     fetchWeatherData(location);
   }, [weatherK]);
 
-  const handleLocationChange = (event) => {
-    let valueToCheck = event.target.value;
-    valueToCheck =
-      valueToCheck.charAt(0).toUpperCase() +
-      valueToCheck.slice(1).toLowerCase();
-    //check if non letter char are been typed
-    let res = /[0-9]/.test(valueToCheck);
-    let res2 = /[\/`¬!"£$%^&*()_+=\-\/|\\<>\~}{@:{\[\];'#.\/]/g.test(
-      valueToCheck
-    );
-    //remove spaces
-    let stringWithNoSpaces = valueToCheck.replace(/\s+/g, "");
-
-    if (res || res2) {
-      return setIsInputCorrect(false);
-    } else {
-      setIsInputCorrect(true);
-    }
-
-    setLocation(stringWithNoSpaces);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetchWeatherData(location);
-  };
-
+  //this part set the time and date
   const settingClockToTime = function (timeZone, unixTimestamp) {
     let timeOfZone = fromUnixTime(unixTimestamp + timeZone).toUTCString();
     setMyDate(timeOfZone);
   };
 
   useEffect(() => {
-    settingClockToTime(timeZone, unixTimestamp);
+    settingClockToTime(timezoneFromData, unixtimestampFromData);
   }, [weatherData]);
 
-  if (!weatherData || !hourlyDailyWeather) {
+  //if there is no secondData this means that the page is still loading
+  if (!hourlyDailyWeather) {
     return (
       <div id="loadingPage">
         Loading, please wait
@@ -80,21 +76,61 @@ const WeatherDisplay = () => {
     );
   }
 
+  //this handle the user input
+  const handleLocationChange = (event) => {
+    let valueToCheck = event.target.value;
+    valueToCheck =
+      valueToCheck.charAt(0).toUpperCase() +
+      valueToCheck.slice(1).toLowerCase();
+    setLocation(valueToCheck);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchWeatherData(location);
+  };
+
+  //check for errors
+  if (error) {
+    console.log(error);
+    return (
+      <div className="formAndAll">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Location:
+            <input
+              type="text"
+              value={location}
+              required
+              minLength="3"
+              maxLength="15"
+              onChange={handleLocationChange}
+            ></input>
+          </label>
+          <button type="submit">
+            Search
+            <img
+              alt="search icon"
+              width={15}
+              height={15}
+              src="https://www.pinclipart.com/picdir/middle/395-3952831_search-search-icon-vector-png-clipart.png"
+            />
+          </button>
+          <p>
+            for a more precise location or if you don't find what you're looking
+            for enter ",COUNTRY" ex. `Rome,IT`
+          </p>
+        </form>
+        <h1>{error}</h1>
+      </div>
+    );
+  }
+
+  //if no error is found deconstruct weatherData and show info
   const { name, main, weather } = weatherData;
   const { daily, hourly } = hourlyDailyWeather;
   const dailyArrayWithId = daily.map((obj, index) => ({ ...obj, id: index }));
   dailyArrayWithId.shift();
-
-  const isThereAnError = weatherData.message;
-  if (!isThereAnError) {
-    console.log("no error");
-  } else {
-    return (
-      <div className="errorInCityPage">
-        Erorr <p>{isThereAnError}</p>
-      </div>
-    );
-  }
 
   const dailyDom = dailyArrayWithId.map((day) => {
     if (day.message) {
@@ -128,7 +164,7 @@ const WeatherDisplay = () => {
   });
 
   return (
-    <div>
+    <div className="main">
       <div className="formAndAll">
         <form onSubmit={handleSubmit}>
           <label>
@@ -157,46 +193,24 @@ const WeatherDisplay = () => {
           </p>
         </form>
       </div>
-      <div>
-        {weatherData.message ? (
-          <div className="noResponseFromApi">
-            <h2>
-              there was no result for your search, check for any spelling
-              mistakes
-            </h2>
-          </div>
-        ) : (
-          <div>
-            {isInputCorrect ? (
-              <div className="info">
-                <h2>{name}</h2>
-                <p>{weather[0].main}</p>
-                <p>{weather[0].description}</p>
-                <p>Temperature: {main.temp}°C</p>
-                <p>Feels like: {main.feels_like}°C</p>
-                <p>Humidity:{main.humidity}%</p>
-                <img
-                  src={`http://openweathermap.org/img/w/${weather[0].icon}.png`}
-                  alt={weather[0].description}
-                />
-                <p>Precipitations: {Math.floor(daily[0].pop * 100)}%</p>
+      <div className="info">
+        <h2>{name}</h2>
+        <p>{weather[0].main}</p>
+        <p>{weather[0].description}</p>
+        <p>Temperature: {main.temp}°C</p>
+        <p>Feels like: {main.feels_like}°C</p>
+        <p>Humidity:{main.humidity}%</p>
+        <img
+          src={`http://openweathermap.org/img/w/${weather[0].icon}.png`}
+          alt={weather[0].description}
+        />
+        <p>Precipitations: {Math.floor(daily[0].pop * 100)}%</p>
 
-                <div>{myDate ? <h1>{myDate}</h1> : <p>loading time</p>}</div>
-                <p>{dailyDom}</p>
-                <p>{hourlyDom}</p>
-              </div>
-            ) : (
-              <div>
-                <h2>
-                  Input is incorrect, check if it contains numbers or special
-                  characters
-                </h2>
-              </div>
-            )}
-          </div>
-        )}
+        <div>{myDate ? <h1>{myDate}</h1> : <p>loading time</p>}</div>
+        <p>{dailyDom}</p>
+        <p>{hourlyDom}</p>
       </div>
-      );
+      )}
     </div>
   );
 };
